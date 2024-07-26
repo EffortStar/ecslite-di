1ec913fd2433390a8ef690c6eed9d7ea5c0747fa
Thomas Ingram <thomasingram@vertx.xyz>
2024-07-26 15:49:52 +1000

    Added MeansImplicitUse to [Di] to suppress readonly warnings.
    Added changelog and updated package version.

--- DiAttribute.cs
+++ DiAttribute.cs
@@ -1,7 +1,8 @@
 using System;
+using JetBrains.Annotations;
 
 namespace EffortStar.EcsLite.Di {
-  [AttributeUsage(AttributeTargets.Field)]
+  [AttributeUsage(AttributeTargets.Field), MeansImplicitUse(ImplicitUseKindFlags.Assign)]
   public class DiAttribute : Attribute {
     public string? WorldName;
   }
